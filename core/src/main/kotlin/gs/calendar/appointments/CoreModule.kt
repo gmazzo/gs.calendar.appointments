9d7c445da2b57b5f7dd199cd4dab89efddaf42b8
Guillermo Mazzola <gmazzo65@gmail.com>
2019-02-24 22:15:34 -0300

    Fixed build issues

--- core/src/main/kotlin/gs/calendar/appointments/CoreModule.kt
+++ core/src/main/kotlin/gs/calendar/appointments/CoreModule.kt
@@ -1,14 +1,11 @@
 package gs.calendar.appointments
 
 import com.google.api.client.auth.oauth2.Credential
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
 import com.google.api.client.http.HttpTransport
 import com.google.api.client.json.JsonFactory
-import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.services.calendar.Calendar
 import dagger.Module
 import dagger.Provides
-import dagger.Reusable
 import gs.calendar.appointments.agendas.AgendasModule
 import gs.calendar.appointments.auth.AuthModule
 import gs.calendar.appointments.events.EventsModule
@@ -27,15 +24,5 @@ internal class CoreModule {
         Calendar.Builder(transport, jsonFactory, credential)
             .setApplicationName(applicationName)
             .build()!!
-
-    @Provides
-    @Reusable
-    fun provideHttpTransport(): HttpTransport =
-        GoogleNetHttpTransport.newTrustedTransport()
-
-    @Provides
-    @Reusable
-    fun provideJsonFactory(): JsonFactory =
-        JacksonFactory.getDefaultInstance()
 
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ val kotlinVersion: String by settings
 pluginManagement {
     resolutionStrategy {
         eachPlugin {
-            if (requested.id.namespace?.startsWith("org.jetbrains.kotlin") == true) {
+            if (requested.id.id.startsWith("org.jetbrains.kotlin.")) {
                 useVersion(kotlinVersion)
             }
         }
