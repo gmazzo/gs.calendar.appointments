b24a219cf1d178b9c54cb2a10326e784703a799e
Guillermo Mazzola <gmazzo65@gmail.com>
2019-03-28 09:53:34 -0300

    Added CSS baseline

--- frontend/src/main/kotlin/gs/calendar/appointments/frontend/Main.kt
+++ frontend/src/main/kotlin/gs/calendar/appointments/frontend/Main.kt
@@ -1,5 +1,6 @@
 package gs.calendar.appointments.frontend
 
+import material_ui.core.uiCssBaseline
 import react.dom.render
 import kotlin.browser.document
 import kotlin.browser.window
@@ -7,6 +8,8 @@ import kotlin.browser.window
 fun main() {
     window.onload = {
         render(document.getElementById("root")!!) {
+            uiCssBaseline {
+            }
             app()
         }
     }
---
+++ frontend/src/main/kotlin/material_ui/core/CssBaseline.kt
@@ -0,0 +1,9 @@
+@file:JsModule("@material-ui/core")
+
+package material_ui.core
+
+import react.RClass
+import react.RProps
+
+@JsName("CssBaseline")
+external val uiCssBaseline: RClass<RProps>
